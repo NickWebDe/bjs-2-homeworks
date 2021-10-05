998d19f50c2ac8cb6691576958667ceeea43ff52
Nikita Lashik <lnv.mfg@gmail.com>
2021-10-06 00:13:12 +0300

    homeworks Object

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,34 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+let oleg = new Student('Олег', 'Male', 18);
+let anton = new Student('Антон', 'Male', 22);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark)
+  }
+  
+}
+
+Student.prototype.addMarks = function (...args) {
+    this.marks = args;
+}
+
+Student.prototype.getAverage = function () {
+return this.marks.reduce((previousValue, currentValue) => previousValue + currentValue) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason)  {
+  delete this.marks, this.subject;
+  this.excluded = reason;
+}
