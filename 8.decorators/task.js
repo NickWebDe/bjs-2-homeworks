e6f66c66aa609412e8e8bb98a2b6518777c28e77
Nikita Lashik <lnv.mfg@gmail.com>
2021-11-14 15:20:09 +0300

    Домашнее задание Функции Декораторы

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,24 @@
-function cachingDecoratorNew(func) {
-  // Ваш код
+function cachingDecoratorNew (func) {
+  let cache = [
+  ];
+  const maxCache = 5;
+  const idxFlag = -1;
+  
+  function wrapper (...args) {
+    const hash = args.join(',')
+    const idx = cache.findIndex((element) => element.hash === hash);
+    if(idx != idxFlag) {
+      console.log(`Из кэша: ${cache[idx].value}`);
+    } else {
+      let result = func(...args);
+      cache.push({ hash: hash, value : result});
+      console.log(`Вычисляем: ${result}`);
+      if(cache.length > maxCache) {
+        cache.shift();
+      }
+    }
+
+  }
+  return wrapper;
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
-
-function debounceDecorator2(func) {
-  // Ваш код
-}
